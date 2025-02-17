449963ed31b899a2abc3a976fba21fbd62dca885
MdTanzil <mdtanzil.py@gmail.com>
2025-02-18 01:17:39 +0600

    som

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,53 +4,57 @@ import { Box, Button, Container, Typography } from "@mui/material";
 import React from "react";
 const Hero = (): React.FC => {
   return (
-    <Box
-      sx={{
-        minHeight: "55vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        color: "white",
-        textAlign: "center",
-      }}
-    >
-      <Container>
-        <Typography variant="h3" fontWeight="bold" className="text-6xl ">
-          Discover & Collect <br />
-          Extraordinary NFTs
-        </Typography>
-        <Typography sx={{ mt: 2, opacity: 0.8 }}>
-          Enter the world of digital art and collectibles. Explore unique NFTs
-          created by artists worldwide.
-        </Typography>
-        <Box sx={{ mt: 4, display: "flex", gap: 2, justifyContent: "center" }}>
-          {/* Gradient Button */}
-          <Button
-            variant="contained"
-            sx={{
-              borderRadius: "999px",
-              backgroundColor: "#925aef ",
-              gap: 1,
-            }}
+    <>
+      <Box
+        sx={{
+          minHeight: "55vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          color: "white",
+          textAlign: "center",
+        }}
+      >
+        <Container>
+          <Typography variant="h3" fontWeight="bold" className="text-6xl ">
+            Discover & Collect <br />
+            Extraordinary NFTs
+          </Typography>
+          <Typography sx={{ mt: 2, opacity: 0.8 }}>
+            Enter the world of digital art and collectibles. Explore unique NFTs
+            created by artists worldwide.
+          </Typography>
+          <Box
+            sx={{ mt: 4, display: "flex", gap: 2, justifyContent: "center" }}
           >
-            <RocketLaunchIcon /> Start Creating
-          </Button>
-          {/* Outlined Button */}
-          <Button
-            variant="contained"
-            sx={{
-              backgroundColor: "#374151",
-              color: "white",
-              gap: 1,
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <PlayCircleOutlineIcon /> Watch Demo
-          </Button>
-        </Box>
-      </Container>
-    </Box>
+            {/* Gradient Button */}
+            <Button
+              variant="contained"
+              sx={{
+                borderRadius: "999px",
+                backgroundColor: "#925aef ",
+                gap: 1,
+              }}
+            >
+              <RocketLaunchIcon /> Start Creating
+            </Button>
+            {/* Outlined Button */}
+            <Button
+              variant="contained"
+              sx={{
+                backgroundColor: "#374151",
+                color: "white",
+                gap: 1,
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <PlayCircleOutlineIcon /> Watch Demo
+            </Button>
+          </Box>
+        </Container>
+      </Box>
+    </>
   );
 };
 
